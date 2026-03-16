581978168c5f8d0304c9d3bf25c473cc49540453
KABOSS <kasperkb94@gmail.com>
2026-03-16 12:40:12 +0100

    Updated PFR based on base class

--- src/process_engineering_toolkit/reactors/pfr.py
+++ src/process_engineering_toolkit/reactors/pfr.py
@@ -1,1 +1,29 @@
-# PFR
+from scipy.integrate import solve_ivp
+
+from .base import Reactor
+
+
+class PFR(Reactor):
+
+    def solve(self, V_span):
+
+        F0 = self.build_inlet_vector()
+
+        def ode(V, F):
+
+            r = self.reaction_rates(F)
+
+            dFdV = self.SM @ r
+
+            return dFdV
+
+        sol = solve_ivp(
+            ode,
+            V_span,
+            F0,
+            method="BDF"
+        )
+
+        self.solution = sol
+        return sol
+    
