37ef172afbf28fa859a239b3f0629a4a947dcf22
KABOSS <kasperkb94@gmail.com>
2026-03-27 17:40:19 +0100

    Stripped PFR

--- src/process_engineering_toolkit/reactors/pfr.py
+++ src/process_engineering_toolkit/reactors/pfr.py
@@ -30,46 +30,4 @@ class PFR(Reactor):
         }
 
         return self.solution
-
-    # def volume_for_conversion(self, species, X_target, n_points=500):
-    #     """
-    #     Compute the reactor volume required to reach a given conversion of `species`.
-    #     """
-
-    #     if self.solution is None:
-    #         raise RuntimeError("Reactor must be solved first")
-
-    #     # Get profile on a dense grid
-    #     profile = self.profile(species_for_conversion=species, n_points=n_points)
-    #     X = profile["conversion"]
-    #     V = profile["volume"]
-
-    #     # Interpolate conversion as a function of volume
-    #     f = interp1d(X, V, bounds_error=True)
-
-    #     # Compute required volume
-    #     V_required = f(X_target)
-
-    #     return float(V_required)
-
-    # def conversion_at_volume(self, species, V_input, n_points=500):
-    #     """
-    #     Conversion of `species` achievable at a reactor volume `V`.
-    #     """
-
-    #     if self.solution is None:
-    #         raise RuntimeError("Reactor must be solved first")
-
-    #     # Get profile on a dense grid
-    #     profile = self.profile(species_for_conversion=species, n_points=n_points)
-    #     X = profile["conversion"]
-    #     V = profile["volume"]
-
-    #     # Interpolate volume as a function of conversion
-    #     f = interp1d(V, X, bounds_error=True)
-
-    #     # Compute achievable conversion
-    #     X_V = f(V_input)
-
-    #     return float(X_V)
     
