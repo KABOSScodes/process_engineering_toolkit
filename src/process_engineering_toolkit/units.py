3f700c78a48908da4b917f5de081ac656cf62579
KABOSS <kasperkb94@gmail.com>
2026-04-22 14:05:29 +0200

    Added a function to attach units to a profile object

--- src/process_engineering_toolkit/units.py
+++ src/process_engineering_toolkit/units.py
@@ -1,5 +1,6 @@
 # units.py
 from pint import UnitRegistry
+import numpy as np
 
 ureg = UnitRegistry()
 Q_ = ureg.Quantity
@@ -16,3 +17,17 @@ def standardize_units(value, expected_unit): # Pint can parse strings, so allow 
         return value.to(expected_unit).magnitude
     else:
         return value  # assume already in SI
+
+def _attach_units(profile, units_map): # Should remove _
+    def recurse(value, unit):
+        if value is None:
+            return None
+        elif isinstance(value, dict):
+            return {k: recurse(v, unit) for k, v in value.items()}
+        else:
+            return Q_(value, unit)
+
+    return {
+        key: recurse(value, units_map[key])
+        for key, value in profile.items()
+    }
